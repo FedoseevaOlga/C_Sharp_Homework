14b2b1aaba6c3e86cf395aa6b438b72c70598dfd
Olga <ogata@yandex.ru>
2023-04-08 08:46:33 +0300

    UPD homework #8

--- Program.cs
+++ Program.cs
@@ -1,101 +1,48 @@
-﻿// ДОМАШНЕЕ ЗАДАНИЕ К СЕМИНАРУ 7
+﻿//ДОМАШНЕЕ ЗАДАНИЕ К СЕМИНАРУ 8
+//ЗАДАЧА 54. Задайте двумерный массив размером, упорядочите по убыванию элементы каждой строки.
 
-//ЗАДАЧА 47. Задайте двумерный массив размером m*n, заполненный случайными вещественными числами.
-
-/*Console.Write("Введите количество строк: ");
-int rows = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов: ");
-int columns = int.Parse(Console.ReadLine()!);
-
-double[,] array = GetArrayDouble(rows,columns, -10, 10);
-PrintArrayDouble(array);
-
-// Заполнение двумерного массива
-double[,] GetArrayDouble(int m, int n, int minValue, int maxValue)
-{
-    double[,] res = new double[m,n];
-    for(int i=0; i < m; i++)
-    {
-        for(int j=0; j < n; j++)
-        {res[i, j] = Math.Round(new Random().NextDouble()+ new Random().Next(maxValue-minValue),2);}
-    }
-    return res;
-}
-
-void PrintArrayDouble(double[,] array)
-{for(int i=0; i < array.GetLength(0); i++)
-{ for(int j=0; j < array.GetLength(1); j++){
- Console.Write($"{array[i,j]} "); }
- Console.WriteLine();
- }
-}*/
-
-// ЗАДАЧА 50 Программа принимает на вход два числа, и проверяет, есть ли такая позиция в массиве.
-/*Console.Write("Введите количество строк: ");
-int rows = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов: ");
-int columns = int.Parse(Console.ReadLine()!);
-Console.Write("Введите первое число, i= ");
-int a = int.Parse(Console.ReadLine()!);
-Console.Write("Введите второе число, j = ");
-int b = int.Parse(Console.ReadLine()!);
-
-int[,] array = GetArray(rows,columns, 0, 10);
-PrintArray(array);
-FindElement (array, a, b); 
-
-void FindElement(int [,] array, int a, int b)
-
-{ if(a > array.GetLength(0) || b > array.GetLength(1))
-  Console.WriteLine("такого элемента в массиве нет ");
-  else Console.WriteLine($"Такой элемент есть: {array[a,b]}");
-   
-}
-
-// Заполнение двумерного массива
-int[,] GetArray(int m, int n, int minValue, int maxValue)
-{
-    int[,] res = new int[m,n];
-    for(int i=0; i < m; i++)
-    {
-        for(int j=0; j < n; j++)
-        {res[i, j] = new Random().Next(minValue,maxValue + 1);}
-    }
-    return res;
-}
-
-void PrintArray(int[,] array)
-{for(int i=0; i < array.GetLength(0); i++)
-{ for(int j=0; j < array.GetLength(1); j++){
- Console.Write($"{array[i,j]} "); }
- Console.WriteLine();
- }
-
-}*/
-
-//ЗАДАЧА 52 Найти среднеарифметическое каждого столбца.
 Console.Write("Введите количество строк: ");
 int rows = int.Parse(Console.ReadLine()!);
 Console.Write("Введите количество столбцов: ");
 int columns = int.Parse(Console.ReadLine()!);
 
-
 int[,] array = GetArray(rows,columns, 0, 10);
 PrintArray(array);
-//Console.WriteLine($" Среднеарифметическое каждого столбца: {FindArithmetic(array)}"); 
-Console.WriteLine($"[{String.Join(",", FindArithmetic(array))}]");
-int[] FindArithmetic(int [,] array)
+Console.WriteLine();
 
-{  int[] res = new int[array.GetLength(0)];
-   for(int j=0; j < array.GetLength(1); j++)
+int[,] newArray = Decrease(array);
+PrintArray(newArray);
+
+int [,]  Decrease(int[,] array)
+{
+for(int i=0; i < array.GetLength(0); i++)
+{   
+   
+    int temp = 0;
+    for(int j=0; j < array.GetLength(1); j++)
     {   
-        int sum =0;
-        for(int i=0; i < array.GetLength(0); i++)
-        {sum+=array[i,j];}
-        res [j]=sum/(array.GetLength(0));
+            int max = array[i,j];
+            int imax = j;
+        for(int k=j; k < array.GetLength(1); k++)
+        {   
+           
+            if(array[i,k]>max) 
+            {  
+                max=array[i,k];
+                imax=k;
+            }
+        }
+        //if(i==1) Console.WriteLine($"j={j}; max= {max}; imax= {imax}");
+        temp=array[i,j];
+        array[i,j] = max;
+        array[i,imax] =temp;
+       //if(i==1) PrintArray(array);
     }
-   return res;
+
+} return array;
 }
+
+
 // Заполнение двумерного массива
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -116,3 +63,61 @@ void PrintArray(int[,] array)
  }
 
 }
+
+//ЗАДАЧА 58. Задать и перемножить две матрицы.
+/*Console.Write("Введите количество строк первой матрицы: ");
+int rows1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов первой матрицы: ");
+int columns1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество строк второй матрицы: ");
+int rows2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов второй матрицы: ");
+int columns2 = int.Parse(Console.ReadLine()!);
+
+int[,] array1 = GetArray(rows1,columns1, 0, 10);
+PrintArray(array1);
+int[,] array2 = GetArray(rows2,columns2, 0, 10);
+PrintArray(array2);
+PrintArray(Multiplay(array1,array2));
+
+int[,] Multiplay(int[,] matrix1, int[,] matrix2)
+{   
+    int [,] result = new int[matrix1.GetLength(0),matrix2.GetLength(1)];
+     if(matrix1.GetLength(0)!=matrix2.GetLength(1)) 
+    {Console.WriteLine("Матрицы нельзя перемножить. Количество строк первого множителя должно совпадать с количеством столбцов второго");}
+    else
+    {
+    for(int i=0; i < matrix1.GetLength(0); i++)
+       {
+       for(int j=0; j < matrix2.GetLength(1); j++)
+          { for(int k=0; k < matrix2.GetLength(0); k++)
+              {
+              result[i,j]+= matrix1[i,k]*matrix2[k,j];
+              }
+           }
+        }  
+    }
+    return result;
+}
+
+//Заполнение двумерного массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] res = new int[m,n];
+    for(int i=0; i < m; i++)
+    {
+        for(int j=0; j < n; j++)
+        {res[i, j] = new Random().Next(minValue,maxValue + 1);}
+    }
+    return res;
+}
+
+void PrintArray(int[,] array)
+{for(int i=0; i < array.GetLength(0); i++)
+{ for(int j=0; j < array.GetLength(1); j++){
+ Console.Write($"{array[i,j]} "); }
+ Console.WriteLine();
+ }
+
+}*/
