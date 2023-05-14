d2beba60e83d99ef006c1642634e7b3fb301bd2c
Olga <ogata@yandex.ru>
2023-05-14 19:52:34 +0300

    UPD homework #98

--- Program.cs
+++ Program.cs
@@ -1,123 +1,57 @@
-﻿//ДОМАШНЕЕ ЗАДАНИЕ К СЕМИНАРУ 8
-//ЗАДАЧА 54. Задайте двумерный массив размером, упорядочите по убыванию элементы каждой строки.
+﻿
+//ДОМАШНЕЕ ЗАДАНИЕ К СЕМИНАРУ 9 "РЕКУРСИЯ"
+// Задача 64: Выполнить с помощью рекурсии.Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
 
-Console.Write("Введите количество строк: ");
-int rows = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов: ");
-int columns = int.Parse(Console.ReadLine()!);
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-int[,] array = GetArray(rows,columns, 0, 10);
-PrintArray(array);
-Console.WriteLine();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
 
-int[,] newArray = Decrease(array);
-PrintArray(newArray);
+Console.WriteLine(PrintNumbers(N, 1));
 
-int [,]  Decrease(int[,] array)
-{
-for(int i=0; i < array.GetLength(0); i++)
-{   
-   
-    int temp = 0;
-    for(int j=0; j < array.GetLength(1); j++)
-    {   
-            int max = array[i,j];
-            int imax = j;
-        for(int k=j; k < array.GetLength(1); k++)
-        {   
-           
-            if(array[i,k]>max) 
-            {  
-                max=array[i,k];
-                imax=k;
-            }
-        }
-        //if(i==1) Console.WriteLine($"j={j}; max= {max}; imax= {imax}");
-        temp=array[i,j];
-        array[i,j] = max;
-        array[i,imax] =temp;
-       //if(i==1) PrintArray(array);
-    }
-
-} return array;
+// Метод 
+string PrintNumbers(int start, int end) {
+    if(start==end) return start.ToString();
+    return (start + " " + PrintNumbers(start-1, end));
 }
 
+//Задача 66: Выполнить с помощью рекурсии.
+//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-// Заполнение двумерного массива
-int[,] GetArray(int m, int n, int minValue, int maxValue)
-{
-    int[,] res = new int[m,n];
-    for(int i=0; i < m; i++)
-    {
-        for(int j=0; j < n; j++)
-        {res[i, j] = new Random().Next(minValue,maxValue + 1);}
-    }
-    return res;
-}
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
-void PrintArray(int[,] array)
-{for(int i=0; i < array.GetLength(0); i++)
-{ for(int j=0; j < array.GetLength(1); j++){
- Console.Write($"{array[i,j]} "); }
- Console.WriteLine();
- }
+ /*Console.Write("Введите первое число: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int N = int.Parse(Console.ReadLine()!);
 
-}
+Console.WriteLine(PrintSum(M, N));
 
-//ЗАДАЧА 58. Задать и перемножить две матрицы.
-/*Console.Write("Введите количество строк первой матрицы: ");
-int rows1 = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов первой матрицы: ");
-int columns1 = int.Parse(Console.ReadLine()!);
+// Метод 
+int PrintSum(int M, int N) {
+    if (M == N) return M;                       
+    if (M < N) return N + PrintSum(M, N - 1); 
+    return N + PrintSum(M, N + 1); 
+}*/
 
-Console.Write("Введите количество строк второй матрицы: ");
-int rows2 = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов второй матрицы: ");
-int columns2 = int.Parse(Console.ReadLine()!);
+// Задача 68: Выполнить с помощью рекурсии.
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
 
-int[,] array1 = GetArray(rows1,columns1, 0, 10);
-PrintArray(array1);
-int[,] array2 = GetArray(rows2,columns2, 0, 10);
-PrintArray(array2);
-PrintArray(Multiplay(array1,array2));
+/*Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine()!);
 
-int[,] Multiplay(int[,] matrix1, int[,] matrix2)
-{   
-    int [,] result = new int[matrix1.GetLength(0),matrix2.GetLength(1)];
-     if(matrix1.GetLength(0)!=matrix2.GetLength(1)) 
-    {Console.WriteLine("Матрицы нельзя перемножить. Количество строк первого множителя должно совпадать с количеством столбцов второго");}
-    else
-    {
-    for(int i=0; i < matrix1.GetLength(0); i++)
-       {
-       for(int j=0; j < matrix2.GetLength(1); j++)
-          { for(int k=0; k < matrix2.GetLength(0); k++)
-              {
-              result[i,j]+= matrix1[i,k]*matrix2[k,j];
-              }
-           }
-        }  
-    }
-    return result;
-}
+Console.WriteLine(FindFunction(m, n));
 
-//Заполнение двумерного массива
-int[,] GetArray(int m, int n, int minValue, int maxValue)
-{
-    int[,] res = new int[m,n];
-    for(int i=0; i < m; i++)
-    {
-        for(int j=0; j < n; j++)
-        {res[i, j] = new Random().Next(minValue,maxValue + 1);}
-    }
-    return res;
-}
-
-void PrintArray(int[,] array)
-{for(int i=0; i < array.GetLength(0); i++)
-{ for(int j=0; j < array.GetLength(1); j++){
- Console.Write($"{array[i,j]} "); }
- Console.WriteLine();
- }
-
+// Метод 
+int FindFunction(int m, int n) {
+    if (m == 0) return n+1;                       
+    else if (m>0 && n==0) {return  FindFunction(m-1, 1);}
+    return (FindFunction(m-1, FindFunction(m, n-1)));
 }*/
